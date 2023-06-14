1c835abaf9c2e5771a92ae050e456249d1c06b98
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2023-06-14 20:33:16 -0300

    feat: updated routers

--- src/http/router.ts
+++ src/http/router.ts
@@ -2,25 +2,36 @@ import passport from "passport";
 import { AuthController } from "./controllers/auth-controller";
 import { UserController } from "./controllers/user-controller";
 import { Router } from "express";
+import { MealController } from "./controllers/meal-controller";
 
 export const router = Router();
 
 const userController = new UserController();
+const mealController = new MealController();
 const authController = new AuthController();
 
-router.post("/user", userController.create);
-router.get("/user/:id", passport.authenticate("jwt"), userController.find);
-router.put("/user/:id", passport.authenticate("jwt"), userController.update);
-router.delete("/user/:id", passport.authenticate("jwt"), userController.delete);
-
 router.post("/auth/login", authController.login);
-
+router.get("/google/callback", passport.authenticate("google"), (req, res) => {
+  res.redirect("/");
+  res.send("Logged in!");
+});
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
-router.get("/google/callback", passport.authenticate("google"), (req, res) => {
-  res.redirect("/");
-  res.send("Logged in!");
-});
+router.post("/user", userController.create);
+router.get(
+  "/user/metrics",
+  passport.authenticate("jwt"),
+  userController.metrics
+);
+router.get("/user/:id", passport.authenticate("jwt"), userController.find);
+router.put("/user/:id", passport.authenticate("jwt"), userController.update);
+router.delete("/user/:id", passport.authenticate("jwt"), userController.delete);
+
+router.post("/meal", passport.authenticate("jwt"), mealController.create);
+router.get("/meal/:id", passport.authenticate("jwt"), mealController.find);
+router.get("/meals", passport.authenticate("jwt"), mealController.listByUser);
+router.put("/meal/:id", passport.authenticate("jwt"), mealController.update);
+router.delete("/meal/:id", passport.authenticate("jwt"), mealController.delete);
